9c068e6ef3538b8839057ecd43be5da030a00cc6
Douglas Carvalho Mariano <dgsmrn@outlook.com>
2024-06-29 22:42:58 -0300

    variableScope

---
+++ cap02/11-ES2015-variableScope/11-ES2015-variableScope.js
@@ -0,0 +1,31 @@
+let movie = "Lord of the Rings";
+// var movie = "Batman v Superman"; // erro, movie já foi declarada
+
+function starWarsFan() {
+  const movie = "Star Wars";
+  return movie;
+}
+
+function marvelFan() {
+  movie = "The Avengers";
+  return movie;
+}
+
+function blizzardFan() {
+  const isFan = true;
+  let phrase = "Warcraft";
+  console.log("Before if: " + phrase);
+  if (isFan) {
+    let phrase = "initial text";
+    phrase = "For the Horde!";
+    console.log("Inside if: " + phrase);
+  }
+  phrase = "For the Alliance!";
+  console.log("After if: " + phrase);
+}
+
+console.log(movie);
+console.log(starWarsFan());
+console.log(marvelFan());
+console.log(movie);
+blizzardFan();
---
+++ cap02/11-ES2015-variableScope/readme.md
@@ -0,0 +1,4 @@
+As varíaveis podem ter um escopo dependendo de onde elas foram declaradas. A varíavel const não pode ter seu valor alterado, e a varíavel let pode. Porém elas só existem no escopo em que foram definidas.
+Temos o escopo global, neste exemplo a variavel let movie existe em todo o código.
+Temos o escopo de função, como a const movie na função starWarsFan, só existe dentro da função.
+Temos também o scopo de bloco no exemplo da let phrase que foi criada e só existe dentro do bloco if(isFan).
